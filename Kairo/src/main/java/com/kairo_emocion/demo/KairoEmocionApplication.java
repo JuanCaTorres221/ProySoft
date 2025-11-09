96de7ae58df0c240866ffd1c66f7bf50cf7213d0
JuanCaTorres221 <geforcenecesito@gmail.com>
2025-11-09 18:34:48 -0500

    Se añadieron archivos faltantes de spring

---
+++ Kairo/src/main/java/com/kairo_emocion/demo/KairoEmocionApplication.java
@@ -0,0 +1,13 @@
+package com.kairo_emocion.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairoEmocionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KairoEmocionApplication.class, args);
+	}
+
+}
---
+++ Kairo/src/test/java/com/kairo_emocion/demo/KairoEmocionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairo_emocion.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairoEmocionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
